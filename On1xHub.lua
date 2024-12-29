3b7d8b2e706474923cbedd4f4ddd1405c556e711
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-29 23:28:09 +0200

    Add files via upload

---
+++ On1xHub.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/azurkxo/OnixHub/refs/heads/main/1233123123123.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
