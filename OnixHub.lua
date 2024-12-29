0f9e98c2b32221e6267a80ec687f76385c769b8c
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-29 23:27:04 +0200

    Update OnixHub.lua

--- OnixHub.lua
+++ OnixHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/azurkxo/OnixHub/refs/heads/main/243234234234234234.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/azurkxo/OnixHub/refs/heads/main/1233123123123.lua'))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
